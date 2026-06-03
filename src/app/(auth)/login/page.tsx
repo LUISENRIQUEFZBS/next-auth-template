ea744d75fa38935ad657e7660b6451ec3c5b3837
Luis Enrique <fernandez.luis@pucp.pe>
2026-06-02 21:36:50 -0500

    from mac os

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <button onClick={() => signIn("google", { callbackUrl: "/dashboard" })}>
+        Sign in with Google
+      </button>
+    </div>
+  );
+}
